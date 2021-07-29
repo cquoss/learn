6865088a7838ddee2f96e0548102fdcf6aafb241
Clemens Quoss <clemens@quoss.de>
2021-07-29 11:42:36 +0200

    Adding a navigation component

--- stockz-application/src/app.js
+++ stockz-application/src/app.js
@@ -1,1 +1,24 @@
-console.log("Hello Web Standards");
+class AirNav extends HTMLElement {
+
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const links = this.querySelectorAll("a");
+        console.log(links);
+        links.forEach(e => this.registerListener(e));
+    }
+
+    registerListener(e) {
+        e.onclick = evt => this.onLinkClicked(evt);
+    }
+
+    onLinkClicked(evt) {
+        evt.preventDefault();
+        console.log(evt);
+    }
+
+}
+
+customElements.define('air-nav', AirNav);
